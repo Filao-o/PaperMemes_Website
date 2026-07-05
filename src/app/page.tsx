ab111a1dcbad3fe8640da6d65ab8382c387477a4
Arwox <armougom.u@gmail.com>
2026-07-05 19:43:31 +0400

    Add home page

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Hero from '@/components/Hero';
+import Suite from '@/components/Suite';
+import HowItWorks from '@/components/HowItWorks';
+import WhyUs from '@/components/WhyUs';
+import FAQ from '@/components/FAQ';
+import CTA from '@/components/CTA';
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Suite />
+      <HowItWorks />
+      <WhyUs />
+      <FAQ />
+      <CTA />
+    </main>
+  );
+}
